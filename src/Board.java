c5f659add6bd5a4915533ef28f2c669fbff9fed9
thedonator <thedonator@users.noreply.github.com>
2021-03-18 21:17:59 +0100

    Created java class Board
    Inserted function for initializing players and for
    initializing the game board

---
+++ src/Board.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+
+public class Board {
+  public static int rows = 3;
+  public static int columns = 3;
+
+  public static boolean [][] isEmpty  = new boolean [rows][columns];
+  public static String [][] signs  = new String [rows][columns];
+  public static String [] playerNames = new String [2];
+
+  public static void initializePlayers(Scanner in)
+  {
+    for (int i = 0; i < 2 ; i++) {
+      System.out.println("Insert player name:");
+      playerNames[i] = in.nextLine();
+    }
+  }
+
+  public static void initializeGameBoard(Scanner in)
+  {
+    for (int i = 0; i < columns; i++) {
+      for (int j = 0; j < rows; j++) {
+        isEmpty[i][j] = true;
+        signs[i][j] = " ";
+      }
+    }
+  }
+
+  public static void printBoard()
+  {
+    System.out.println("   1   2   3");
+    System.out.println(" +---+---+---+");
+    System.out.println("1| "+ signs[0][0] +" | "+ signs[0][1]+" | "+ signs[0][2]+" |");
+    System.out.println(" +---+---+---+");
+    System.out.println("2| "+ signs[1][0] +" | "+ signs[1][1]+" | "+ signs[1][2]+" |");
+    System.out.println(" +---+---+---+");
+    System.out.println("3| "+ signs[2][0] +" | "+ signs[2][1]+" | "+ signs[2][2]+" |");
+    System.out.println(" +---+---+---+");
+  }
+
+}
